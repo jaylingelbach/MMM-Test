5619a7925c5bd0aa3fd3935cd7d013f2e277da8d
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-03-01 15:34:00 -0600

    a commit

--- MMM-Test.js
+++ MMM-Test.js
@@ -7,28 +7,24 @@ Module.register("MMM-Test", {
     getDom: function() {
         console.log("A CONSOLE LOG");
         this.getQuote();
-        var element = document.createElement("div")
-        element.className = "myContent"
-        element.innerHTML = `Hello, ${starterText}!`
-        return element
+        var element = document.createElement("div");
+        element.className = "myContent";
+        element.innerHTML = `Hello, ${this.defaults.starterText}!`;
+        return element;
     },
 
     getQuote: async function () {
         try {
-          const url = "https://stoic.tekloon.net/stoic-quote";
-          const res = await fetch(url , {
-            method: "GET",
-            mode:"no-cors"
-          });
-          const data = await res.json();
-          console.log("data", data);
-          return data; // Ensure the function returns the fetched data
+            const url = "https://stoic.tekloon.net/stoic-quote";
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log("data", data);
+            return data;
         } catch (error) {
-          console.error("Error:", error);
-          throw error; // Re-throw if you want the caller to handle it
+            console.error("Error:", error);
         }
-      },
+    },
+
     notificationReceived: function() {},
     socketNotificationReceived: function() {},
-
-    })
+});
