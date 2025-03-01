345471c5a721758906a4498832f23d628addba5a
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-03-01 13:19:00 -0600

    added quote function

--- MMM-Test.js
+++ MMM-Test.js
@@ -4,13 +4,26 @@ Module.register("MMM-Test", {
     
     getDom: function() {
         console.log("A CONSOLE LOG");
+        this.getQuote();
         var element = document.createElement("div")
         element.className = "myContent"
         element.innerHTML = "Hello, World!"
         return element
     },
+
+    getQuote: async function () {
+        try {
+          const url = "https://stoic.tekloon.net/stoic-quote";
+          const res = await fetch(url);
+          const data = await res.json();
+          console.log("data", data);
+          return data; // Ensure the function returns the fetched data
+        } catch (error) {
+          console.error("Error:", error);
+          throw error; // Re-throw if you want the caller to handle it
+        }
+      },
     notificationReceived: function() {},
     socketNotificationReceived: function() {},
 
-    
     })
