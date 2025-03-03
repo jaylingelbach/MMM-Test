116be7731f2fc99929a5e36478d377ac63f9c6a3
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-03-02 18:37:54 -0600

    more basics

--- MMM-Test.js
+++ MMM-Test.js
@@ -4,31 +4,21 @@ Module.register("MMM-Test", {
     },
     getDom: function() {
         var element = document.createElement("div")
-        element.className = "myContent"
-        element.innerHTML = "Hello, World! " + this.config.foo
-        var subElement = document.createElement("p")
-        subElement.id = "COUNT"
-        element.appendChild(subElement)
+        element.innerHTML = "Hello, World! " + this.config.starterText
         return element
       },
       
-      notificationReceived: function(notification, payload, sender) {
-        switch(notification) {
-          case "DOM_OBJECTS_CREATED":
-            var timer = setInterval(()=>{
-              this.sendSocketNotification("DO_YOUR_JOB", this.count)
-              this.count++
-            }, 1000)
-            break
-        }
-      },
-      
-      socketNotificationReceived: function(notification, payload) {
-        switch(notification) {
-          case "I_DID":
-            var elem = document.getElementById("COUNT")
-            elem.innerHTML = "Count:" + payload
-            break
-        }
-      },
+    notificationReceived: function(notification, payload, sender) {
+    switch(notification) {
+        case "DOM_OBJECTS_CREATED":
+        var timer = setInterval(()=>{
+            this.sendSocketNotification("DO_YOUR_JOB", this.count)
+            this.count++
+        }, 1000)
+        break
+    }
+    },
+    getHeader: function() {
+    return "Jay's first module"
+    }
 });
