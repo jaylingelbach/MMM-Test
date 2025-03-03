d4b8a03dce85a032d0ee21f3c905f5754b5c1cea
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-03-03 11:59:37 -0600

    added log back into node helper

--- node_helper.js
+++ node_helper.js
@@ -4,5 +4,6 @@ require("dotenv").config();
 module.exports = NodeHelper.create({
   start: function () {
     console.log("Starting node_helper for MMM-Test");
+    console.log("@!@!@!@!@!@!@!@!@!@!@!", process.env);
   }
 });
