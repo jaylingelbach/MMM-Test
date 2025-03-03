1cddcc56872d0a2ebb1c050be34923bcb3d18b19
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-03-02 20:18:33 -0600

    more fuckery 9

--- MMM-Test.js
+++ MMM-Test.js
@@ -1,50 +1,86 @@
-let topPosts = "";
+// /* MagicMirror²
 
 Module.register("MMM-Test", {
-    defaults: {
-        text: "Jay",
-        apiBase: "https:reddit.com/r/programming/top.json?t=week&limit=3"
-    },
+ // Module config defaults.
+ defaults: {
+   //update every 3 minutes
+   updateInterval: 180000,
+   //fade speed
+   fadeSpeed: 4000,
+   //initial load delay
+   initialLoadDelay: 0,
+   //retry delay
+   retryDelay: 2500,
+   //api url
+   apiBase: "https://api.urbandictionary.com/v0/random"
+ },
+ getHeader: function () {
+   return "urbandictionary.com Random Word";
+ },
+ getStyles: function () {
+   return [
+     "style.css" // will try to load it from the vendor folder, otherwise it will load is from the module folder.
+   ];
+ },
 
-    getTopPosts: async function() {
-        fetch(this.config.apiBase)
-            .then((res) => res.json())
-            .then(console.log)
-            .catch(console.error);
-    //    const response =  await fetch(this.config.apiBase, {
-    //     method: "GET",
-    //     mode:"cors",
-    //     headers: {
-    //         "Content-Type": "application-json"
-    //     }
-    //    });
-    //    const res = await response.json();
-    //     Log.Log("!!!!!!!!! RES:  ", res);
-            // data.children.forEach((post) => {
-            //     topPosts += post.data.title + "<br/>";
-           
-        //})
-    },
-    getDom: function() {
-        var element = document.createElement("div")
-        this.getTopPosts();
-        element.innerHTML = topPosts;
-        return element
-      },
-      
-    notificationReceived: function(notification, payload, sender) {
-    switch(notification) {
-        case "DOM_OBJECTS_CREATED":
-        Log.log(`Dom objects created... Can manipulate the DOM now.  Notification: ${notification}`)
-        break
-    }
-    if (sender) {
-        Log.log(`Sender: ${sender.name}, notification: ${notification} `)
-    } else {
-        Log.log(`No sender, Payload: ${payload}`)
-    }
-    },
-    getHeader: function() {
-    return "Jay's first module"
-    }
+ // Define start sequence.
+ start: function () {
+   Log.info("Starting module: " + this.name);
+
+   this.lastComplimentIndex = -1;
+   this.word = "";
+   // Schedule update timer.
+   setInterval(() => {
+     this.updateDom(this.config.fadeSpeed);
+   }, this.config.updateInterval);
+ },
+
+ /**
+  * Retrieve the word of the day along with description, example, and author.
+  *
+  * @returns {string[]} array with word, description, example, and author
+  */
+ getWord: async function () {
+   const response = await fetch(this.config.apiBase);
+   const json = await response.json();
+   const word = json.list[0].word;
+   //This removes  the brackets from definition and example and it limits the amount of characters to 130 for each so they fit nice.
+   const definition = json.list[0].definition
+     .replace(/[\[\]]/g, "")
+     .substr(0, 130);
+   const example = json.list[0].example.replace(/[\[\]]/g, "").substr(0, 130);
+   const author = json.list[0].author;
+   return [word, definition, example, author];
+ },
+
+ // Override dom generator.
+ getDom: function () {
+   const wrapper = document.createElement("div");
+   wrapper.className = this.config.classes
+     ? this.config.classes
+     : "urban bright pre-line";
+   //wrapper.innerHTML = this.config.word;
+   this.getWord().then((response) => {
+     wrapper.innerHTML =
+       "<div class='word'>" +
+       response[0] +
+       "</div>" +
+       "<div class='description'>" +
+       "<strong>Description: </strong>" +
+       response[1] +
+       "</div>" +
+       "<br>" +
+       "<div class='example'>" +
+       "<strong>Example: </strong>" +
+       "<em>" +
+       response[2] +
+       "</em>" +
+       "</div>" +
+       "<br>" +
+       "Author: " +
+       response[3];
+   });
+   //wrapper.innerHTML = this.config.word;
+   return wrapper;
+ }
 });
